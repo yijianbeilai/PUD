27ca6413eaaf5ea18fc2840eb162ca19f25cc4fd
wjz <2534890334@qq.com>
2019-10-11 17:38:04 +0800

    更新测试类

--- src/main/java/com/hz/practisemybatis/controller/TestController.java
+++ src/main/java/com/hz/practisemybatis/controller/TestController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @RequestMapping("test1")
     public void test1(){
+        int i=1;
+        i+=1;
         System.out.println("Hello World!!!");
     }
 }
